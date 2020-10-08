516e5e6118d965a5d759c3f30a92af00040590b6
seanlu99 <seanlu99@gmail.com>
2020-10-08 18:35:55 -0400

    Add timezone middleware

---
+++ swickapp/middleware.py
@@ -0,0 +1,15 @@
+import pytz
+
+from django.utils import timezone
+
+class TimezoneMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated and hasattr(request.user, 'restaurant'):
+            tzname = request.user.restaurant.timezone
+            timezone.activate(pytz.timezone(tzname))
+        else:
+            timezone.deactivate()
+        return self.get_response(request)
